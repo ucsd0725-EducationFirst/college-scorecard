958deca40d0e9b98368630d05d8cfcb576a82eab
Kai Wells <support@kaiwells.me>
2017-09-21 20:59:09 -0700

    Harder, better, faster, stronger

--- scorecard_api.js
+++ scorecard_api.js
@@ -1,75 +1,130 @@
-var ScorecardRequest = function() {
-    var api_key = "w8UC6PgtWN6vn1eAVhLraLW3xwpBlCkO6UnHy8zI";
-    this.url = "https://api.data.gov/ed/collegescorecard/v1/schools?api_key=" + api_key;
-    this.fields = new Array();
-
-    this.onlyFields = function(fields) {
-        this.url += "&_fields=" + fields;
-        this.fields = fields.split(",");
-        return this;
+var Scoreboard = function() {
+    var config = {
+        apiKey: "AIzaSyBxpTELNwV5kDCAQXTpG-lMzWnIbzcNazs",
+        authDomain: "bentham-endurance.firebaseapp.com",
+        databaseURL: "https://bentham-endurance.firebaseio.com",
+        projectId: "bentham-endurance",
+        storageBucket: "bentham-endurance.appspot.com",
+        messagingSenderId: "305674895801"
     };
+    firebase.initializeApp(config);
+    this.database = firebase.database();
+    var self = this;
 
-    this.sortedBy = function(param) {
-        this.url += "&_sort=" + param;
-        return this;
-    };
-
-    this.radiusAround = function(zip, distance) {
-        this.url += "&_zip=" + zip + "&_distance=" + distance;
-        return this;
+    this.byField = function(field) {
+        var q = new ScoreboardQuery(self.database);
+        q.field = field;
+        return q;
     }
 
-    this.get = function(callback, fail) {
-        var fail = fail || function(error) { console.log(error); }
-        var self = this;
-        $.get(this.url)
-        .done(function(data) {
-            var meta = data.metadata;
-            var results = data.results;
-            var schools = [];
-            results.forEach(function(data) {
-                schools.push(new ScoreboardSchoolResult(data));
-            });
-            callback(schools);
-        })
-        .fail(fail)
-        return this;
-    };
-};
+    this.byState = function(state) {
+        var q = new ScoreboardQuery(self.database);
+        q.state = state;
+        return q;
+    }
+}
 
-var ScoreboardSchoolResult = function(json) {
-    this.raw = json;
+var ScoreboardQuery = function(database) {
+    var self = this;
+    this.database = database;
 
-    var toLookFor = [
-        ["name", ["school", "name"]],
-        ["url", ["school", "school_url"]],
-        ["lat", ["location", "lat"]],
-        ["long", ["location", "lon"]],
-        ["zipCode", ["school", "zip"]],
-        ["city", ["school", "city"]],
-        ["state", ["school", "state"]],
-        ["region", ["school", "region_id"]],
-        ["male", ["2014", "student", "demographics", "men"]],
-        ["female", ["2014", "student", "demographics", "female_share"]]
-    ];
+    this.byField = function(field) {
+        self.field = field;
+        return self;
+    }
 
-    for (var i = 0; i < toLookFor.length; i++) {
-        var property = toLookFor[i][0];
-        var path = toLookFor[i][1];
-        var result = json;
-        var foundFlag = true;
-        for (var j = 0; j < path.length; j++) {
-            var element = path[j];
-            try {
-                result = result[element];
-            } catch (err) {
-                // console.log(element, err);
-                foundFlag = false;
-                break;
+    this.byState = function(state) {
+        self.state = state;
+        return self;
+    }
+
+    this.get = function(callback) {
+        function getSchools(schools, callback) {
+            var numSchools = Object.keys(schools).length;
+            for (var id in schools) {
+               self.database.ref("/schools")
+                            .child(id)
+                            .once("value")
+                            .then(function(snapshot) {
+                                schools[snapshot.key].school = snapshot.val();
+                                numSchools--;
+                            });
+            }
+            var loop = setInterval(function() {
+                if (numSchools < 1) {
+                    clearInterval(loop);
+                    callback(schools);
+                }
+            }, 100);
+        }
+
+        if (self.field && self.state) {
+            self.database.ref("/fields")
+                         .child(self.field)
+                         .orderByChild("state")
+                         .equalTo(self.state)
+                         .once("value")
+                         .then(function(snapshot) {
+                             getSchools(snapshot.val(), callback);
+                         });
+        } else if (self.field) {
+            self.database.ref("/fields")
+                         .child(self.field)
+                         .once("value")
+                         .then(function(snapshot) {
+                             getSchools(snapshot.val(), callback);
+                         });
+        } else if (self.state) {
+            self.database.ref("/schools")
+                         .orderByChild("state")
+                         .equalTo(self.state)
+                         .once("value")
+                         .then(function(snapshot) {
+                             getSchools(snapshot.val(), callback);
+                         });
+        } else {
+            throw new Error("ScoreboardQuery error: not enough parameters");
+        }
+    }
+}
+
+function FilterChildrenByPropertyPath(object, path, filter) {
+    var results = {};
+    overObjects:
+    for (var id in object) {
+        var child = object[id];
+        var value = JSON.parse(JSON.stringify(child));
+        for (var i = 0; i < path.length; i++) {
+            var p = path[i];
+            if (value[p]) {
+                value = value[p];
+            } else {
+                continue overObjects;
             }
         }
-        if (foundFlag) {
-            this[property] = result;
+        if (filter(value)) {
+            results[id] = child;
         }
     }
+    return results;
+}
+
+function OrderChildrenByPropertyPath(object, path, ascending) {
+    // Sorts with biggest values first by default
+    ascending = ascending || false;
+    var ordered = [];
+    for (var key in object) {
+        var child = object[key];
+        child.key = key;
+        ordered.push(child);
+    }
+    ordered.sort(function(a, b) {
+        var a_value; var b_value;
+        path.forEach(function(p) {
+            if (a[p]) { a = a[p]; } else { return -1; }
+            if (b[p]) { b = b[p]; } else { return 1; }
+        });
+        return ascending ? (a > b) : (a < b);
+    });
+    return ordered;
 }
